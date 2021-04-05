6a62edfe0fcd03c8c4a8ad738a0743d7b1965719
Melchior MAJ <mmaj@e1r12p2.42.fr>
2021-04-05 15:27:30 +0200

    print_contact

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 16:49:56 by mmaj              #+#    #+#             */
-/*   Updated: 2021/04/05 10:09:53 by mmaj             ###   ########.fr       */
+/*   Updated: 2021/04/05 15:27:12 by mmaj             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,19 @@ void    ft_add(int *index, contact *contact_tab)
 
 std::string	transform_string(std::string str)
 {
+	std::string str2;
+	
 	if (str.length() > 10)
 	{
-		std::string::iterator it = str;
+		str2 = str.substr(0, 10);
+		std::string::iterator it = str2.begin();
+		it[9] = '.';
+		return (str2);
 	}
-	return ();
+	return (str);
 }
 
-void	ft_print_contact(int index, contact *contact_tab)
+void	ft_print_all_contact(int index, contact *contact_tab)
 {
 	int		i = 0;
 
@@ -57,37 +62,48 @@ void	ft_print_contact(int index, contact *contact_tab)
 	{	
 		std::cout << std::setfill (' ') << std::setw (10);
 		std::cout << i + 1;
-		std::cout << std::setfill (' ') << std::setw (11);
-		std::cout << "LOOONG";
-		std::cout << std::setfill (' ') << std::setw (11);
-		std::cout << "GNE";
-		std::cout << std::setfill (' ') << std::setw (11);
-		std::cout << "BLA";
+		std::cout << "|";
+		std::cout << std::setfill (' ') << std::setw (10);
+		std::cout << transform_string(contact_tab[i].getFirstName());
+		std::cout << "|";
+		std::cout << std::setfill (' ') << std::setw (10);
+		std::cout << transform_string(contact_tab[i].getLastName());
+		std::cout << "|";
+		std::cout << std::setfill (' ') << std::setw (10);
+		std::cout << transform_string(contact_tab[i].getNickName());
 		std::cout << std::endl;
 		i++;
 	}
+}
 
-
-	// while (i < index)
-	// {
-	// 	std::cout << i + 1;
-	// 	std::cout << std::setw(10);
-	// 	std::cout << contact_tab[i].getFirstName();
-	// 	std::cout << contact_tab[i].getLastName();
-	// 	std::cout << contact_tab[i].getNickName();
-	// 	std::cout << std::endl;
-	// 	i++;
-	// }
+void	ft_print_contact(int index, contact *contact_tab)
+{
+	
 }
 
 void    ft_search(int *index, contact *contact_tab)
 {
-	// if (*index == 0) {
-	// 	std::cout << "\nplease add contact first\n" << std::endl;
-	// 	return ;
-	// }
-	ft_print_contact(*index, contact_tab);
-	std::cout << "Please choose an index\n" << std::endl;
+	int			input_int;
+
+	if (*index == 0) {
+		std::cout << "\nplease add contact first\n" << std::endl;
+		return ;
+	}
+	ft_print_all_contact(*index, contact_tab);
+	std::cout << "\nPlease choose an index\n" << std::endl;
+	std::cin >> input_int;
+	if (std::cin.fail() || input_int < 1 || input_int > *index)
+	{
+		std::cout << "Error" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+		std::cout << "The index you choose doesn't exist" << std::endl;
+		return ;
+	}
+	std::cout << "index is : " << input_int << std::endl;
+    std::cin.clear();
+    std::cin.ignore(256,'\n');
+	ft_print_contact();
 }
 
 void    phonebook(void)
@@ -109,9 +125,6 @@ void    phonebook(void)
             break;
         else
             std::cout << command << " is not a command" << std::endl;
-
-		// std::cout << "CHECK FIRST NAME : " << contact_tab[0].getFirstName() << std::endl;
-
         std::cout << "Please enter command (ADD/SEARCH/EXIT)" << std::endl;
     }
 }
