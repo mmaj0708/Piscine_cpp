18d33ecf590e1aa1cffa881eeea2b017dd3e2cc2
Melchior MAJ <mmaj@e2r4p14.42.fr>
2021-04-13 17:15:09 +0200

    ending cpp_03

---
+++ cpp_03/ex00/FragTrap.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/13 14:23:15 by mmaj              #+#    #+#             */
+/*   Updated: 2021/04/13 14:57:20 by mmaj             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : Name("default FragTrap")
+{
+	 std::cout << "default constructor called" << std::endl;
+	 return;
+}
+
+FragTrap::FragTrap( std::string _Name ) : Name(_Name) 
+{
+	std::cout << "parametric constructor called" << std::endl;
+	return;
+}
+
+FragTrap::FragTrap( FragTrap const & src )
+{
+	 std::cout << "copy constructor called" << std::endl;
+	 *this = src;
+	 return;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "destructor called" << std::endl;
+	return;
+}
+
+FragTrap & FragTrap::operator=( FragTrap const & rhs )
+{
+	std::cout << "assignation operator called" << std::endl;
+	this->Hit_points = rhs.Hit_points;
+	this->Max_hit_points = rhs.Max_hit_points;
+	this->Energy_points = rhs.Energy_points;
+	this->Max_energy_points = rhs.Max_energy_points ;
+	this->Level = rhs.Level;
+	this->Melee_attack_damage = rhs.Melee_attack_damage;
+	this->Ranged_attack_damage = rhs.Ranged_attack_damage;
+	this->Armor_damage_reduction = rhs.Armor_damage_reduction;
+    return (*this);
+}
+
+std::ostream & operator<<( std::ostream & o, FragTrap const & rhs )
+{
+	o << "name :\t\t\t " << rhs.Name << std::endl;
+	o << "level :\t\t\t " << rhs.Level << std::endl;
+	o << "hit_points :\t\t " << rhs.Hit_points << std::endl;
+	o << "energy_points :\t\t " << rhs.Energy_points << std::endl;
+	o << "max_hit_points :\t " << rhs.Max_hit_points << std::endl;
+	o << "melee_attack_damage :\t " << rhs.Melee_attack_damage << std::endl;
+	o << "range_attack_damage :\t " << rhs.Ranged_attack_damage << std::endl;
+	o << "armor_damage_reduction : " << rhs.Armor_damage_reduction << std::endl;
+	return (o);
+}
---
+++ cpp_03/ex00/FragTrap.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/13 14:21:09 by mmaj              #+#    #+#             */
+/*   Updated: 2021/04/13 14:48:45 by mmaj             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAG_TRAP
+# define FRAG_TRAP
+
+#include <iostream>
+
+class FragTrap
+{
+private:
+
+public:
+
+	FragTrap( void );
+	FragTrap( std::string Name );
+	FragTrap( FragTrap const & src );
+	~FragTrap( void );
+	FragTrap & operator=( FragTrap const & rhs );
+
+	void	rangedAttack(std::string const & target);
+	void	meleeAttack(std::string const & target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+
+	std::string		Name;
+	int 			Hit_points = 100;
+	int				Max_hit_points = 100;
+	int				Energy_points = 100;
+	int				Max_energy_points = 100;
+	int				Level = 1;
+	int				Melee_attack_damage = 30;
+	int				Ranged_attack_damage = 20;
+	int				Armor_damage_reduction = 5;
+
+};
+
+#endif
