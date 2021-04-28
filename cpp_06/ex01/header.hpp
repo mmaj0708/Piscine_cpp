65ddfbbb2ef2c9f169c4638da694413a34f3c2b0
Melchior MAJ <mmaj@e2r3p14.42.fr>
2021-04-28 16:13:49 +0200

    06_ex01 end

--- cpp_06/ex01/header.hpp
+++ cpp_06/ex01/header.hpp
@@ -6,21 +6,16 @@
 /*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 11:48:50 by mmaj              #+#    #+#             */
-/*   Updated: 2021/04/28 15:27:16 by mmaj             ###   ########.fr       */
+/*   Updated: 2021/04/28 16:05:11 by mmaj             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_HPP
 # define HEADER_HPP
 
-#include <climits>
 #include <iostream>
-#include <float.h>
-#include <iomanip>
+#include <cstring>
+#include <string>
 #include <cstdlib>
-#define ASCII_MIN_PRINTABLE 33
-#define ASCII_MAX_PRINTABLE 126
-
-void	convert(std::string, int, int);
 
 #endif
---
+++ cpp_06/ex01/main.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/28 14:46:10 by mmaj              #+#    #+#             */
+/*   Updated: 2021/04/28 16:11:24 by mmaj             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.hpp"
+
+struct Data
+{
+	std::string s1;
+	int n;
+	std::string s2;
+};
+
+char *str_rand(void)
+{
+	char *str = new char[9];
+
+	for(int i = 0 ; i < 8 ; i++)
+	{
+		while (isalnum(str[i] = rand() % 256) == 0)
+		{}
+	}
+	return (str);
+}
+
+void * serialize(void)
+{
+	char	*serialStr = new char[20];
+	char	*s1 = str_rand();
+	char	*s2 = str_rand();
+	int		n = rand();
+	int		*n_ptr = &n;
+
+	memcpy(serialStr, reinterpret_cast<void *>(s1), 8);	
+	memcpy(&serialStr[8], reinterpret_cast<void *>(n_ptr), 4);	
+	memcpy(&serialStr[12], reinterpret_cast<void *>(s2), 8);	
+	
+
+	delete s1;
+	delete s2;
+	return (static_cast<void*>(serialStr));
+}
+
+Data * deserialize(void * raw)
+{
+	Data *data_ret = new Data;
+	char *ptrRaw   = reinterpret_cast<char*>(raw);
+	char	temp[9];
+
+	temp[8] = '\0';
+	data_ret->s1 = static_cast<char*>(memcpy(temp, static_cast<char*>(ptrRaw), 8));
+	data_ret->n = *(reinterpret_cast<int*>(ptrRaw + 8));
+	data_ret->s2 = static_cast<char*>(memcpy(temp, static_cast<char*>(ptrRaw + 12), 8));
+	return (data_ret);
+}
+
+int main ()
+{
+	Data *d1 = deserialize(serialize());
+	Data *d2 = deserialize(serialize());
+	Data *d3 = deserialize(serialize());
+	Data *d4 = deserialize(serialize());
+
+	std::cout << "|---- D1 ----|" << std::endl;
+	std::cout << "s1 = " << d1->s1 << std::endl;
+	std::cout << "n = " << d1->n << std::endl;
+	std::cout << "s2 = " << d1->s2 << std::endl;
+	std::cout << "|---- D2 ----|" << std::endl;
+	std::cout << "s1 = " << d2->s1 << std::endl;
+	std::cout << "n = " << d2->n << std::endl;
+	std::cout << "s2 = " << d2->s2 << std::endl;
+	std::cout << "|---- D3 ----|" << std::endl;
+	std::cout << "s1 = " << d3->s1 << std::endl;
+	std::cout << "n = " << d3->n << std::endl;
+	std::cout << "s2 = " << d3->s2 << std::endl;
+
+	delete d1;
+	delete d2;
+	delete d3;
+	delete d4;
+	return (0);
+}
