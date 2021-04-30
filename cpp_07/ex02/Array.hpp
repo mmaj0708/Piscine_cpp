97cd986dcc01c900945c0707cb4d34cf98199e75
Melchior MAJ <mmaj@e2r3p14.42.fr>
2021-04-30 12:35:22 +0200

    06/02 gne

---
+++ cpp_07/ex02/Array.hpp
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/30 10:15:04 by mmaj              #+#    #+#             */
+/*   Updated: 2021/04/30 12:24:39 by mmaj             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY
+# define ARRAY
+
+#include <iostream>
+
+template< typename T >
+class Array
+{
+private:
+
+	int		_size;
+	T		*_arr;
+
+public:
+
+	Array( void ); // array vide
+	Array( unsigned int n ); // init un array de taille n avec init a 0 ? ou mets tout a n ?
+	Array( Array const & src );
+	~Array( void );
+	Array & operator=( Array const & rhs );
+	T	& operator[]( unsigned int const & i );
+
+	unsigned int	size() const;
+
+};
+
+// default
+template< typename T >
+Array<T>::Array() : _size(0), _arr()
+{
+	return;
+}
+
+// param
+template <typename T>
+Array<T>::Array(unsigned int n) : _size(n), _arr(new T[n])
+{
+	return ;
+}
+
+// copie
+template <typename T>
+Array<T>::Array( Array const & src )
+{
+	*this = src;
+}
+
+// assign
+template <typename T>
+Array<T> & Array<T>::operator=( Array const & rhs )
+{
+	this->_size = rhs.size();
+
+	this->_arr = new T[_size];
+	for(int i = 0 ; i < _size ; i++)
+		_arr[i] = rhs[i];
+
+	return (*this);
+}
+
+// []
+template <typename T>
+T	& Array<T>::operator[]( unsigned int const & i )
+{
+	if (i >= size())
+		throw std::exception();
+
+	return (_arr[i]);	
+}
+
+// size
+template< typename T >
+unsigned int	Array<T>::size() const
+{
+	return (this->_size);
+}
+
+// destruct
+template< typename T >
+Array<T>::~Array()
+{
+	return;
+}
+
+#endif
--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -6,10 +6,25 @@
 /*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 13:38:09 by mmaj              #+#    #+#             */
-/*   Updated: 2021/04/29 13:46:16 by mmaj             ###   ########.fr       */
+/*   Updated: 2021/04/30 12:21:06 by mmaj             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
 int main( void ) {
 
+	Array<int> tab(5);
+
+	tab[0] = 0;
+	tab[1] = 1;
+	tab[2] = 2;
+	tab[3] = 3;
+	tab[4] = 4;
+
+	std::cout << tab[0] << std::endl;
+	std::cout << tab[1] << std::endl;
+	std::cout << tab[2] << std::endl;
+	std::cout << tab[3] << std::endl;
+	std::cout << tab[4] << std::endl;
 }
